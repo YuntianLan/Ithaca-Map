4c5198aa96e5b3e0c7d7b69470c739dd25e8370a
Yuntian Lan <yl653@cornell.edu>
2018-03-22 22:12:20 -0400

    init mli

---
+++ server.mli
@@ -0,0 +1,29 @@
+module type MapTree = sig
+	type t
+
+	val init_tree : string -> t
+	val get_image_dir : float -> float -> int -> string
+
+end
+
+module type MapGraph = sig
+	type node
+	type edge
+	type t
+
+	val init_graph : string -> t
+
+	val get_node_by_coord : float -> float -> t -> node
+	val get_node_by_name : string -> node
+
+	val shortest_path : node -> node -> node list
+
+	val node_to_coord : node -> (float * float)
+
+end
+
+module type Server = sig
+	type t
+
+	val 
+end
