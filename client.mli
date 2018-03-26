6cb684b802aa9270fbf76a2a9a09cd492ed6ff19
Xinzhe Yang <xinzhe.y@gmail.com>
2018-03-25 21:50:55 -0400

    Trie

--- client.mli
+++ client.mli
@@ -11,3 +11,40 @@ module type GUI = sig
 
 
 end
+
+
+
+module type Trie = sig
+
+  (* [t] is the type of Trie *)
+  type t
+
+  (* [value] is the type of values in the Trie*)
+  type value
+
+  (* [empty] is the empty Trie *)
+  val empty : t
+
+  (* [is_empty trie] is [true] iff [trie] is empty. *)
+  val is_empty : t -> bool
+
+  (* [size trie] is the number of valid values in [trie]*)
+  val size : t -> int
+
+  (* [insert v trie] is [trie] with [v] present *)
+  val insert : value -> t -> t
+
+  (* [member v trie] is [true] iff [v] is a valid value in [trie] *)
+  val member : value -> t -> bool
+
+  (* [find v trie] is a Trie such that all the children match the prefix [v]*)
+  val find : value -> t -> t
+
+  (* [remove v trie] is a Trie such that not children match the prefix [v]*)
+  val remove : value -> t -> t
+
+  (* [to_list trie] is a list containing all the valid values *)
+  val to_list : t -> value list
+
+
+end
