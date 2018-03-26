9f3b77f54678e36dc234ec67f4fa68c6aa74f161
emilylyu <xl358@cornell.edu>
2018-03-25 22:11:46 -0400

    gui

--- GUI.mli
+++ GUI.mli
@@ -7,7 +7,32 @@
  *)
 
 module type GUI = sig
-	type t
+  (* type state =
+    | Init
+    | pattern ->  *)
 
+  (* [init_window] initializes the whole user interface. *)
+  val init_window : unit->unit
+
+  (* [zoom in] choose a picture of larger scale to display when the zoom in
+   * button is clicked. *)
+  val zoom_in : unit->unit
+
+  (* [zoom out] choose a picture of smaller scale to display when the zoom in
+   * button is clicked. *)
+  val zoom_out : unit->unit
+
+  (* [show route] plots the route from start location to destination on the
+   * picture of the map. *)
+  val show_route : unit->unit
+
+  (* [search] choose a picture of smaller scale to display when the zoom
+   * in button is clicked. *)
+  val search : unit->unit
+
+  (* [showLocation] shows a node on the map that represents the user's current
+   * location
+   * in button is clicked. *)
+  val showLoc : unit->unit
 
 end
