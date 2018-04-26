7fd23e0d31231f0d4bc950e58ef19ccc2ce0ed4e
Yuntian Lan <yl653@cornell.edu>
2018-04-26 16:08:33 -0400

    merging

--- server.ml
+++ server.ml
@@ -32,21 +32,33 @@ let init_server =
 	{imagetree = tr; mapgraph = gr}
 
 
+let make_buffer n =
+	let rec make_help n acc = 
+		if n = 0 then acc
+		else make_help (n-1) (" "^acc) in
+	Bytes.of_string (make_help n "")
+
+
 let rec handle_client tg desc = 
-	let str = Bytes.of_string "          " in
-	let len = Unix.recv desc str 0 10 [] in
+	let str = make_buffer 1024 in
+	let len = Unix.recv desc str 0 1024 [] in
 	let sstr = Bytes.to_string str in
 	let _ = print_endline sstr in
 	let _ = Unix.send_substring desc sstr 0 10 [] in
 	handle_client tg desc
 
+let rec test s () =
+	let _ = print_endline s in
+	let _ = Thread.delay 2. in
+	let _ = Thread.yield () in
+	test s ()
 
 let begin_service tg = 
 	let server_socket = 
 		Unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
 	let _ = Unix.bind server_socket 
 		(Unix.ADDR_INET(sip, sport)) in
-	let _ = Unix.listen server_socket 1 in
+	let _ = Unix.listen server_socket 2 in
 	let desc, addr = Unix.accept server_socket in
 	handle_client tg desc
 
