7a1403ad2f8d36cf815bd137dd504a88c6495d72
Xinzhe Yang <xinzhe.y@gmail.com>
2018-04-28 11:44:11 -0400

    uPdate

--- gui/gui.ml
+++ gui/gui.ml
@@ -2,6 +2,33 @@ open Js_of_ocaml
 open Js_of_ocaml_lwt
 open Js
 
+
+(* ========= constants ========== *)
+let max_depth = 6
+let min_depth = 1
+let delta_zoom = 0.04
+let delta_base_move = 0.03
+let init_upleft_lon = -76.5527
+let init_upleft_lat = 42.4883
+let init_lowright_lon = -76.4649
+let init_lowright_lat = 42.4235
+
+
+type params = {
+  upleft_lon: float;
+  upleft_lat: float;
+  lowright_lon: float;
+  lowright_lat: float;
+  width: float;
+  height: float;
+}
+(* TODO *)
+type gui_state = {
+  params: params;
+  current_depth: int;
+}
+
+
 (* [fail] is a failure/exception handler *)
 let fail = fun _ -> assert false
 
@@ -9,12 +36,7 @@ module Html = Dom_html
 let js = Js.string
 let doc = Html.document
 
-(* let Html.createImg doc = Html.createImg doc
-let Html.createDiv doc = Html.createDiv doc
-let createSpan = Html.createSpan doc
-let createInput = Html.createInput doc
-let createLabel = Html.createLabel doc
-let createAttr = Html.createA doc *)
+
 
 let setClass elt s = elt##className <- js s
 let setId elt s = elt##id <- js s
@@ -25,9 +47,11 @@ let append_text e s = Dom.appendChild e (doc##createTextNode (js s))
 let get_element_by_id id =
   Js.Opt.get (Html.document##getElementById (js id)) fail
 
-
+(* onload _ loads all the required HTML elements upon GUI launching *)
 let onload _ =
   (* let doc = Html.document in *)
+
+  (* ==================== begin div map-container ==================== *)
 
   let div_map_container = Html.createDiv doc in
   setClass div_map_container "map-container";
@@ -44,9 +68,12 @@ let onload _ =
   img_map##src <- js "../tiles/1.png";
   Dom.appendChild div_mapbody img_map;
 
+  (* ==================== end div map-container ==================== *)
+
   let img_dest = Html.createImg doc in
   setId img_dest "dest";
   Dom.appendChild doc##body img_dest;
+
   let div_markers = Html.createDiv doc in
   setId div_markers "markers";
   Dom.appendChild doc##body div_markers;
@@ -66,15 +93,18 @@ let onload _ =
   let span_search_container = Html.createSpan doc in
   setClass span_search_container "search-container";
   Dom.appendChild div_card_content span_search_container;
+
   let label_for_tags = Html.createLabel doc in
   label_for_tags##htmlFor <- js "tags";
   Dom.appendChild span_search_container label_for_tags;
+
   let input_search = Html.createInput doc in
   setClass input_search "search";
   setId input_search "tags";
   input_search##placeholder <- js "Search locations";
   Dom.appendChild span_search_container input_search;
 
+  (* ==================== begin icons ==================== *)
 
   let span_icons_container = Html.createSpan doc in
   setClass span_icons_container "icons-container";
@@ -95,6 +125,7 @@ let onload _ =
 
   let a_info = Html.createA doc in
   setClass a_info "info";
+
   let i_question = Html.createI doc in
   setClass i_question "action-icon fa fa-2x fa-question-circle";
   Dom.appendChild a_info i_question;
@@ -104,6 +135,7 @@ let onload _ =
   append_text div_info_text "You can also use arrow keys and -/= to zoom, or use the mouse drag and scroll wheel.
   Double click to begin routing & double click again to end and show route.";
   Dom.appendChild a_info div_info_text;
+
   let div_info_subtext = Html.createDiv doc in
   setClass div_info_subtext "info-subtext";
   append_text div_info_subtext "( click this bubble to close )";
@@ -113,6 +145,14 @@ let onload _ =
   Dom.appendChild span_icons_container a_zoomout;
   Dom.appendChild span_icons_container a_info;
 
+  a_info##onclick <- Dom_html.handler
+      (fun _ ->
+         if div_info_text##style##display = Js.string "none" then
+           (div_info_text##style##display <- Js.string "block";Js._true)
+         else
+           (div_info_text##style##display <- Js.string "none";Js._true));
+
+  (* ==================== end icons ==================== *)
 
   let div_nothing = Html.createDiv doc in
   Dom.appendChild div_actions div_nothing;
@@ -122,9 +162,6 @@ let onload _ =
   append_text a_clear "clear route";
   a_clear##onclick <- Html.handler
     (fun _ ->
-       (* state := not !state;
-       b##.value := if !state then txt2 else txt1;
-          action !state; *)
         input_search##value <- js "";
        Js._true);
   Dom.appendChild div_nothing a_clear;
@@ -157,5 +194,6 @@ let onload _ =
   Dom.appendChild doc##body button; *)
   Js._false
 
+let read_lrlat () =
 let () =
   Dom_html.window##onload <- Dom_html.handler onload
