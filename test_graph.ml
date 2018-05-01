80eb089c878dbe007fee704bc82bb5bfde0247f1
蒋涵清 <hj284@cornell.edu>
2018-04-30 23:09:37 -0400

    add graph function test

--- test_graph.ml
+++ test_graph.ml
@@ -21,26 +21,30 @@ let test_get_node_by_coord = [
   "test4" >:: (fun _ -> assert_equal (42.5043439, -76.3593372)
               (Map.get_node_by_coord 100.0 (30.0) t |> Map.node_to_coord)); 
 
-  "test5" >:: (fun _ -> assert_equal (42.5043439, -76.3593372)
-              (Map.get_node_by_coord （-60.0） (30.0) t |> Map.node_to_coord)); 
+  "test5" >:: (fun _ -> assert_equal (42.318172, -76.374253)
+              (Map.get_node_by_coord  (-60.0) (30.0) t |> Map.node_to_coord)); 
 
-  "test6" >:: (fun _ -> assert_equal (42.5043439, -76.3593372)
-              (Map.get_node_by_coord （-60.0） (-30.0) t |> Map.node_to_coord)); 
+  "test6" >:: (fun _ -> assert_equal (42.318172, -76.374253)
+              (Map.get_node_by_coord  (-60.0) (-30.0) t |> Map.node_to_coord)); 
 
-  "test7" >:: (fun _ -> assert_equal (42.5043439, -76.3593372)
+  "test7" >:: (fun _ -> assert_equal (42.318172, -76.374253)
               (Map.get_node_by_coord 41.0 (-70.0) t |> Map.node_to_coord)); 
 
-  "test8" >:: (fun _ -> assert_equal (42.5043439, -76.3593372)
-              (Map.get_node_by_coord 43.0 (-80.0) t |> Map.node_to_coord));   
+  "test8" >:: (fun _ -> assert_equal (42.7875307, -76.7166654)
+              (Map.get_node_by_coord 43.0 (-80.0) t |> Map.node_to_coord)); 
 ]
 
 
 (* Test get_node_by_name *)
 
 let test_get_node_by_name = [
-  "test1" >:: (fun _ -> assert_equal (42.375543, -76.415228) 
-              (Map.get_node_by_coord 42.40 (-76.3) t |> Map.node_to_coord));
- 
+  "test1" >:: (fun _ -> assert_equal (42.4451475, -76.481154)
+              (match (Map.get_node_by_name "Gates Hall" t) with 
+              | Some x -> Map.node_to_coord x | None -> (0.0,0.0)));
+
+(*   "test2" >:: (fun _ -> assert_equal (42.4281548, -76.4691226)
+              (match (Map.get_node_by_name "Hillside Alliance Church" t) with 
+              | Some x -> Map.node_to_coord x | None -> (0.0,0.0))); *)
 
 ]
 
