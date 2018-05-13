96ef1184e8c04a23ddfc142e615e9d21b8443e5a
Xinzhe Yang <xinzhe.y@gmail.com>
2018-05-13 00:54:10 -0400

    对接

--- gui/gui1.ml
+++ gui/gui1.ml
@@ -473,6 +473,19 @@ let split_coord_list (s:string) : (float*float) list =
     ) params in
   tups
 
+(* [split_coord_name_list s] is the list of coordinate tuples parsed from [s]
+ * requries: [s] must be in the form "coord1,coord2,name1;coord3,coord4,name2;..."*)
+let split_coord_name_list (s:string) : ((float*float)*string) list =
+  let params = String.split_on_char ';' s in
+  let tups = List.map (fun i ->
+      let coords = String.split_on_char ',' i in
+      let res_lat = List.nth coords 0 |> float_of_string in
+      let res_lon = List.nth coords 1 |> float_of_string in
+      let name = List.nth coords 2 in
+      ((res_lat, res_lon), name)
+    ) params in
+  tups
+
 let http_get_nodes_by_name id name coord_to_markers addbutton div_map_container =
   let url = base_url^"?index=2"^"&name="^name in
   let start () =
@@ -511,7 +524,7 @@ let http_get_res st callback canvas context div_map_container =
             "&lowright_lon="^round st.params.param_lowright_lon^
             "&width="^round st.params.width^
             "&height="^round st.params.height in
-  let _ = Dom_html.window##alert(js url) in
+  (* let _ = Dom_html.window##alert(js url) in *)
   let start () =
     http_get url >>= (fun res ->
 
@@ -593,6 +606,27 @@ let http_get_res st callback canvas context div_map_container =
         (* img_path := res; *)
         Lwt.return ()) in
   ignore(start ())
+
+
+let http_get_nodes_by_type id type_name coord_to_markers addbutton div_map_container =
+  let url = base_url^"?index=7"^"&type="^type_name in
+  let start () =
+    http_get url >>= (fun res ->
+        res |> split_coord_name_list
+        addbutton div_map_container;
+        Lwt.return ()) in
+  ignore(start ())
+
+
+
+
+
+
+
+
+
+
+
 (* ========= HTTP requests ========== *)
 (* onload _ loads all the required HTML elements upon GUI launching *)
 let onload _ =
@@ -1093,7 +1127,7 @@ let onload _ =
   append_text a_go "walk";
   a_go##onclick <- Html.handler
       (fun _ ->
-         draw_background_with_line canvas context (st.tx, st.ty) 
+         draw_background_with_line canvas context (st.tx, st.ty)
           (coordinates |> coord_tup_to_markers);
          Js._true);
   Dom.appendChild div_nothing a_go;
